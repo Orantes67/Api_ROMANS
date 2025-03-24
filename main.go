29cc804dc0ef2ff62dc4499a521fb2b1c353f22a
Hector <orantesroman39@gmail.com>
2025-03-23 23:20:41 -0600

    create main

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+
+import (
+	"ApiProduct/src/pagos/infraestructure"
+    perfumesInfra "ApiProduct/src/perfumes/infraestructure"
+	"github.com/gin-gonic/gin"
+	perfumespersonalizadosInfra"ApiProduct/src/perfumes_personalizados/infraestructure"
+)
+	
+
+func main() {
+	r := gin.Default()
+	infraestructure.Init(r)
+	perfumesInfra.Init(r)
+	perfumespersonalizadosInfra.Init(r)
+	r.Run(":8080")
+	
+}
